474bd1f52ffcb913b0c9020fbf17401c3d2e3066
Игорь Рябухин <igorr1970@mail.ru>
2024-11-12 00:02:00 +0500

    Update module_10_5.py

--- module_10_5.py
+++ module_10_5.py
@@ -14,14 +14,12 @@ def read_info(name):
 if __name__ == '__main__':
     filenames = [f'./file {number}.txt' for number in range(1, 5)]
 
-    # Линейный вызов
     start_time = time.time()
     for filename in filenames:
         read_info(filename)
     linear_duration = time.time() - start_time
     print(f"{linear_duration} (линейный)")
 
-    # Многопроцессный вызов
     start_time = time.time()
     with multiprocessing.Pool() as pool:
         pool.map(read_info, filenames)
