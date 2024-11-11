662df85b4dfa38efb67de99f17f23dc4418f7939
Игорь Рябухин <igorr1970@mail.ru>
2024-11-11 21:30:44 +0500

    Add files via upload

---
+++ module_10_1.py
@@ -0,0 +1,43 @@
+import threading
+from time import sleep, time
+
+# Функция для записи слов в файл
+def write_words(word_count, file_name):
+    with open(file_name, 'w') as f:
+        for i in range(1, word_count + 1):
+            f.write(f"Какое-то слово № {i}\n")
+            sleep(0.1)  # Задержка 0.1 секунды
+    print(f"Завершилась запись в файл {file_name}")
+
+# Измерение времени выполнения функции
+start_time = time()
+
+# Вызов функции write_words с заданными параметрами
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+
+end_time = time()
+print(f"Работа функций {end_time - start_time:.6f} секунд")
+
+# Измерение времени выполнения потоков
+start_time_threads = time()
+
+# Создание потоков для записи в файлы
+threads = []
+threads.append(threading.Thread(target=write_words, args=(10, 'example5.txt')))
+threads.append(threading.Thread(target=write_words, args=(30, 'example6.txt')))
+threads.append(threading.Thread(target=write_words, args=(200, 'example7.txt')))
+threads.append(threading.Thread(target=write_words, args=(100, 'example8.txt')))
+
+# Запуск потоков
+for thread in threads:
+    thread.start()
+
+# Ожидание завершения всех потоков
+for thread in threads:
+    thread.join()
+
+end_time_threads = time()
+print(f"Работа потоков {end_time_threads - start_time_threads:.6f} секунд")
