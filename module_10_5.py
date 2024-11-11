dbea577de707f6a19f8e7194e2f1dff538787f66
Игорь Рябухин <igorr1970@mail.ru>
2024-11-12 00:01:23 +0500

    Add files via upload

---
+++ module_10_5.py
@@ -0,0 +1,29 @@
+import time
+import multiprocessing
+
+def read_info(name):
+    all_data = []
+    with open(name, 'r') as file:
+        while True:
+            line = file.readline()
+            if not line:  # End of file
+                break
+            all_data.append(line.strip())
+    return all_data
+
+if __name__ == '__main__':
+    filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+    # Линейный вызов
+    start_time = time.time()
+    for filename in filenames:
+        read_info(filename)
+    linear_duration = time.time() - start_time
+    print(f"{linear_duration} (линейный)")
+
+    # Многопроцессный вызов
+    start_time = time.time()
+    with multiprocessing.Pool() as pool:
+        pool.map(read_info, filenames)
+    parallel_duration = time.time() - start_time
+    print(f"{parallel_duration} (многопроцессный)")
