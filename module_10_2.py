cfcdb882bdbf7f1fa6872a6e797352800e2caf4d
Игорь Рябухин <igorr1970@mail.ru>
2024-11-11 21:50:09 +0500

    Update module_10_2.py

--- module_10_2.py
+++ module_10_2.py
@@ -1,24 +1,22 @@
 import threading
 import time
 
-
 class Knight(threading.Thread):
     def __init__(self, name, power):
         super().__init__()
         self.name = name
         self.power = power
-        self.enemies = 100  # Начальное количество врагов
+        self.enemies = 100
         self.days = 0
 
     def run(self):
         print(f"{self.name}, на нас напали!")
 
         while self.enemies > 0:
-            time.sleep(1)  # Задержка в 1 секунду (1 день)
+            time.sleep(1)
             self.days += 1
             self.enemies -= self.power
 
-            # Убедимся, что количество врагов не становится отрицательным
             if self.enemies < 0:
                 self.enemies = 0
 
@@ -27,15 +25,14 @@ class Knight(threading.Thread):
         print(f"{self.name} одержал победу спустя {self.days} дней(дня)!")
 
 
-# Создание объектов рыцарей
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight('Sir Galahad', 20)
 
-# Запуск потоков
+
 first_knight.start()
 second_knight.start()
 
-# Ожидание завершения битв
+
 first_knight.join()
 second_knight.join()
 
