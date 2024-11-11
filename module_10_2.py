8ecc67e60b3f2a1a3b286cac83364666d7ab82b7
Игорь Рябухин <igorr1970@mail.ru>
2024-11-11 21:51:47 +0500

    Update module_10_2.py

--- module_10_2.py
+++ module_10_2.py
@@ -24,14 +24,11 @@ class Knight(threading.Thread):
 
         print(f"{self.name} одержал победу спустя {self.days} дней(дня)!")
 
-
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight('Sir Galahad', 20)
 
-
 first_knight.start()
 second_knight.start()
-
 
 first_knight.join()
 second_knight.join()
